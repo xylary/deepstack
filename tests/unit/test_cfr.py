06efe76e8728cd30e61043040753e6cbe5ad21d9
Chris Nicholls <cgnicholls@googlemail.com>
2018-02-11 14:16:35 +0000

    Added example test

--- tests/unit/test_cfr.py
+++ tests/unit/test_cfr.py
@@ -5,6 +5,6 @@ from deepstack import cfr
 
 def test_cfr_compute_average_strategy():
     action_counts = {1: {1: 3, 2: 5}, 2: {2: 5, 3: 7}}
-    expected = {1: {1: 3.0/5.0, 2: 5.0/3.0}, 2: {2: 5.0/12.0, 3: 7.0/12.0}}
+    expected = {1: {1: 3.0/8.0, 2: 5.0/8.0}, 2: {2: 5.0/12.0, 3: 7.0/12.0}}
     computed = cfr.compute_average_strategy(action_counts)
     assert expected == computed
