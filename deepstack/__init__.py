42e7a6dd33e47d26cac6332c2851260bddeac392
Chris Nicholls <cgnicholls@googlemail.com>
2018-02-11 14:06:07 +0000

    Refactoring to make deepstack a module

---
+++ deepstack/__init__.py
@@ -0,0 +1,7 @@
+import deepstack.best_response
+import deepstack.cfr
+import deepstack.cfr_game
+import deepstack.example_strategy
+import deepstack.extensive_game
+from .leduc import *
+from .one_card_poker import *
---
+++ tests/unit/test_cfr.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pytest
+
+from deepstack import cfr
+
+def test_cfr_compute_average_strategy():
+    action_counts = {1: {1: 3, 2: 5}, 2: {2: 5, 3: 7}}
+    expected = {1: {1: 3.0/5.0, 2: 5.0/3.0}, 2: {2: 5.0/12.0, 3: 7.0/12.0}}
+    computed = cfr.compute_average_strategy(action_counts)
+    assert expected == computed
